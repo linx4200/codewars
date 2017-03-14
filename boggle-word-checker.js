a46bd42988858296d7f3397e2c597fc7c33be893
xinran.lxr <xinran.lxr@alibaba-inc.com>
2017-03-14 22:43:27 +0800

    Passed: 49 Failed: 3

--- boggle-word-checker.js
+++ boggle-word-checker.js
@@ -1,27 +1,26 @@
 // https://www.codewars.com/kata/57680d0128ed87c94f000bfd/
 
 function checkWord( board, word ){
-  const ll = word.length
   let idx = 0
-  let flag = false
   let i = 0
   let j = 0
-  let rows = board.length
-  let columns = board[0].length
+  let b = JSON.parse(JSON.stringify(board))
+  let rows = b.length
+  let columns = b[0].length
   let stack = []
-  let b = board
 
   while (i < rows) {
     if (b[i][j] === word[idx]) {
-      stack.push([i, j])
       const next = word[idx + 1]
-
-      // console.log('==next===', i, j, word[idx], next)
-
-      if (next === undefined) return true
+      console.log('===stack===', i, j, stack)
+      if (next === undefined) {
+        // console.log('==true===')
+        return true
+      }
 
       if (b[i-1] && b[i-1][j-1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i - 1
         j = j - 1
@@ -29,12 +28,14 @@ function checkWord( board, word ){
       }
       if (b[i] && b[i][j-1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         j = j - 1
         continue
       }
       if (b[i+1] && b[i+1][j-1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i + 1
         j = j - 1
@@ -42,18 +43,21 @@ function checkWord( board, word ){
       }
       if (b[i-1] && b[i-1][j] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i - 1
         continue
       }
       if (b[i+1] && b[i+1][j] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i + 1
         continue
       }
       if (b[i-1] && b[i-1][j+1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i - 1
         j = j + 1
@@ -61,50 +65,80 @@ function checkWord( board, word ){
       }
       if (b[i] && b[i][j+1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         j = j + 1
         continue
       }
       if (b[i+1] && b[i+1][j+1] === next) {
         idx++
+        stack.push([i, j])
         b[i][j] = 'null'
         i = i + 1
         j = j + 1
         continue
       }
-      // break
+
+      const last = stack.pop()
+      if (last) {
+        b[i][j] = 'null';
+        [i, j] = last
+        idx = idx - 1
+        b[i][j] = word[idx]
+        continue
+      }
     }
 
-    b = board
-
-    if (j < columns) {
+    idx = 0
+    b = JSON.parse(JSON.stringify(board))
+    // console.log('==b===', b)
+    if (j < columns - 1) {
       j++
     } else {
       i++
       j = 0
     }
   }
-  return flag
+
+  // console.log('==false===')
+  return false
 }
 
+// const testBoard = [
+//   ['E','A','R','A'],
+//   ['N','L','E','C'],
+//   ['I','A','I','S'],
+//   ['B','Y','O','R']
+// ]
+
 const testBoard = [
-  ['E','A','R','A'],
-  ['N','L','E','C'],
-  ['I','A','I','S'],
-  ['B','Y','O','R']
+  ['N','B','R','A'],
+  ['C','R','P','A'],
+  ['L','A','A','P'],
+  ['S','O','A','A']
 ]
 
-const res = checkWord(testBoard, 'C')
-// const res = checkWord(testBoard, 'EAR' ) // true
-// const res = checkWord( testBoard, 'EARS' ) // false
-// const res = checkWord( testBoard, 'BAILER' ) // true 
-// const res = checkWord( testBoard, 'RSCAREIOYBAILNEA' ) // true
-// const res = checkWord( testBoard, 'CEREAL' ) // false
-// const res = checkWord( testBoard, 'ROBES' ) // false
-// const res = checkWord( testBoard, 'BAKER' ) // false
-// const res = checkWord( testBoard, 'CARS' ) // false
+// checkWord( testBoard, 'C') // true
+// checkWord( testBoard, 'EAR' ) // true
+// checkWord( testBoard, 'EARS' ) // false
+// checkWord( testBoard, 'BAILER' ) // true 
+// checkWord( testBoard, 'RSCAREIOYBAILNEA' ) // true
+// checkWord( testBoard, 'CEREAL' ) // false
+// checkWord( testBoard, 'ROBES' ) // false
+// checkWord( testBoard, 'BAKER' ) // false
+// checkWord( testBoard, 'CARS' ) // false
 
-console.log('==res===', res)
+// checkWord( testBoard, 'ARAARAS') // true
+checkWord( testBoard, 'PARAPARAS' ) // true
+// checkWord( testBoard, 'EARS' ) // false
+// checkWord( testBoard, 'BAILER' ) // true 
+// checkWord( testBoard, 'RSCAREIOYBAILNEA' ) // true
+// checkWord( testBoard, 'CEREAL' ) // false
+// checkWord( testBoard, 'ROBES' ) // false
+// checkWord( testBoard, 'BAKER' ) // false
+// checkWord( testBoard, 'CARS' ) // false
+
+// console.log('==res===', res)
 
 
 
