8918b448c69e2296ec0c143410065c8b9ea75d12
xinran.lxr <xinran.lxr@alibaba-inc.com>
2017-03-24 15:49:02 +0800

    done!

--- multiplying-numbers-as-strings.js
+++ multiplying-numbers-as-strings.js
@@ -2,64 +2,30 @@
 
 // 这题要注意超出精读范围的数，不能简单的相乘
 function multiply(a, b) {
-  let aa
-  let bb
-  if (a.length < b.length) {
-    aa = a.split('').reverse()
-    bb = b.split('').reverse()
-  } else {
-    aa = b.split('').reverse()
-    bb = a.split('').reverse()
-  }
-  let base = 0
-  const bl = bb.length
-  aa = aa.map((val) => {
-    const tmp = bb.map((v, i) => {
-      const mul = (+v * +val + +base).toString().split('')
-      if (i === bl - 1) {
-        base = 0
-        return mul
-      }
-      if (mul.length === 1) {
-        base = 0
-        return mul[0]
-      } else {
-        base = mul[0]
-        return mul[1]
-      }
-    })
-    let last = tmp.pop()
-    if (last.length > 1) tmp.push(last[1])
-    tmp.push(last[0])
-    return tmp
-  })
+  const aa = a.split('').reverse()
+  const bb = b.split('').reverse()
+  const res = []
 
-  aa = aa.map((arr, idx) => {
-    return (new Array(idx).fill('0')).concat(arr)
-  })
-  console.log(aa)
-  base = 0
-  const res = aa[aa.length - 1].map((aaa, idx) => {
-    let val = 0
-    aa.map((curr) => {
-      val += (curr[idx] ? +curr[idx] : 0)
-    })
-    val = (+val + +base).toString().split('')
-    if (val.length === 1) {
-      base = 0
-      return val[0]
+  // 先把答案的每一位乘积先算出来，不考虑进位
+  for(let i = 0, l = aa.length; i < l; i++) {
+    for (let j = 0, ll = bb.length; j < ll; j++) {
+      const mul = aa[i] * bb[j]
+      res[i + j] = res[i + j] ? res[i + j] + mul : mul
     }
-    base = val[0]
-    return val[1]
-  }, 0)
+  }
 
+  // 再处理进位
+  for(let k = 0, l = res.length; k < l; k++) {
+    const c = Math.floor(res[k]/10)
+    res[k] = res[k] % 10
+    res[k + 1] = res[k + 1] || 0
+    res[k + 1] += c
+  }
   return res.reverse().join('').replace(/^0+(?=\d)/g, '')
 }
 
-// console.log(multiply('2', '3')) // '6'
-// console.log(multiply('2', '0')) // '0'
-// console.log(multiply('20000', '0')) // '0'
-console.log(multiply('58608473622772837728372827', '7586374672263726736374')) // '444625839871840559930507365023406205566214109298'
-                                                          6205566214109298
-//444625839871840560024489175424316205566214109298
-//44462583987184055993050736502340 6205566214109298
+multiply('2', '3') // '6'
+multiply('2', '0') // '0'
+multiply('20000', '0') // '0'
+multiply('123321', '9088') // '1120741248'
+multiply('58608473622772837728372827', '7586374672263726736374') // '444625839871840560024489175424316205566214109298'
