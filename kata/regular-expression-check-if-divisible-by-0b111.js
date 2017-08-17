9ece8eee57f1b244f56c8e39253f413ad80555a8
liuxinran <liuxinran1008@gmail.com>
2017-08-17 23:29:23 +0800

    give up…

--- kata/regular-expression-check-if-divisible-by-0b111.js
+++ kata/regular-expression-check-if-divisible-by-0b111.js
@@ -5,6 +5,8 @@
 // Run test:
 // npm-run mocha ../test/regular-expression-check-if-divisible-by-0b111.js
 
-const solution = /^[01]+$/
+// 不玩了，知道原理就算了，下面的正则是错的，打死也写不对
+
+const solution = /^(0|1((000)*1|(0(01)*1(0(01)*(111)*0)*(0|(1(000)*1))))(111)*10*)$/
 
 exports.solution = solution
